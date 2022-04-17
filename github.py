b90de6e7e01c22fdf1926aa828c96e9117854a5b
sandeth indisa kongahawatta <74986126+cardison@users.noreply.github.com>
2022-04-17 19:00:21 +0530

    Add files via upload

---
+++ github.py
@@ -0,0 +1,23 @@
+
+
+
+
+print( google)
+
+birth_year=input("hi when is your birthday")
+
+age=2022-int(birth_year)
+
+print(age)
+
+
+weight_pounds=input('type your weight (in pounds)')
+
+weight_kg =int(weight_pounds)* 0.45
+
+print(weight_kg)
+
+
+
+
+
